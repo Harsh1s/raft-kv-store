ab79e671cd46ef8f3426d53eab4e8001530d25e9
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-10-18 13:18:03 +0530

    Set up minikv workspace

--- src/coordinator/metadata.rs
+++ src/coordinator/metadata.rs
@@ -75,3 +75,80 @@ impl MetadataStore {
     pub fn put_key(&self, meta: &KeyMetadata) -> Result<()> {
         let cf = self.db.cf_handle(CF_KEYS).unwrap();
         let value = bincode::serialize(meta)
+            .map_err(|e| crate::Error::Internal(format!("Serialize error: {}", e)))?;
+        self.db.put_cf(cf, meta.key.as_bytes(), value)?;
+        Ok(())
+    }
+
+    #[allow(clippy::result_large_err)]
+    pub fn get_key(&self, key: &str) -> Result<Option<KeyMetadata>> {
+        let cf = self.db.cf_handle(CF_KEYS).unwrap();
+        match self.db.get_cf(cf, key.as_bytes())? {
+            Some(bytes) => {
+                let meta: KeyMetadata = bincode::deserialize(&bytes)
+                    .map_err(|e| crate::Error::MetadataCorrupted(e.to_string()))?;
+                Ok(Some(meta))
+            }
+            None => Ok(None),
+        }
+    }
+
+    #[allow(clippy::result_large_err)]
+    pub fn delete_key(&self, key: &str) -> Result<()> {
+        let cf = self.db.cf_handle(CF_KEYS).unwrap();
+        self.db.delete_cf(cf, key.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn list_keys(&self) -> Result<Vec<String>> {
+        let cf = self.db.cf_handle(CF_KEYS).unwrap();
+        let iter = self.db.iterator_cf(cf, rocksdb::IteratorMode::Start);
+
+        let mut keys = Vec::new();
+        for item in iter {
+            let (key_bytes, _) = item?;
+            let key = String::from_utf8(key_bytes.to_vec())
+                .map_err(|_| crate::Error::MetadataCorrupted("Invalid UTF-8".into()))?;
+            keys.push(key);
+        }
+
+        Ok(keys)
+    }
+
+    pub fn put_volume(&self, meta: &VolumeMetadata) -> Result<()> {
+        let cf = self.db.cf_handle(CF_VOLUMES).unwrap();
+        let value = bincode::serialize(meta)
+            .map_err(|e| crate::Error::Internal(format!("Serialize error: {}", e)))?;
+        self.db.put_cf(cf, meta.volume_id.as_bytes(), value)?;
+        Ok(())
+    }
+
+    pub fn get_volume(&self, volume_id: &str) -> Result<Option<VolumeMetadata>> {
+        let cf = self.db.cf_handle(CF_VOLUMES).unwrap();
+        match self.db.get_cf(cf, volume_id.as_bytes())? {
+            Some(bytes) => {
+                let meta: VolumeMetadata = bincode::deserialize(&bytes)
+                    .map_err(|e| crate::Error::MetadataCorrupted(e.to_string()))?;
+                Ok(Some(meta))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn list_volumes(&self) -> Result<Vec<VolumeMetadata>> {
+        let cf = self.db.cf_handle(CF_VOLUMES).unwrap();
+        let iter = self.db.iterator_cf(cf, rocksdb::IteratorMode::Start);
+
+        let mut volumes = Vec::new();
+        for item in iter {
+            let (_, value_bytes) = item?;
+            let meta: VolumeMetadata = bincode::deserialize(&value_bytes)
+                .map_err(|e| crate::Error::MetadataCorrupted(e.to_string()))?;
+            volumes.push(meta);
+        }
+
+        Ok(volumes)
+    }
+
+    pub fn get_healthy_volumes(&self) -> Result<Vec<VolumeMetadata>> {
+        Ok(self
